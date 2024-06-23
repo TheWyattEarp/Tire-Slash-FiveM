d5ad2cca6e0a2fef4372451fc885ad4e161b4ebe
TheWyattEarp <Riddlejustin10@gmail.com>
2024-06-23 18:41:51 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+games {"gta5"}
+
+--DO NOT REMOVE THESE
+title "TireSlash"
+description "Allows players to slash tires"
+author "WyattEarp"
+
+--THIS IS IMPORTANT
+client_script 'client.lua'
+server_script 'server.lua'
